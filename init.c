92096a924508db18461c701e7a8f5177b48c1bee
joe9 <joe9mail@gmail.com>
2014-08-11 22:35:15 -0500

    updated init and added rc

--- init.c
+++ init.c
@@ -1,23 +1,29 @@
+
+/* got the below code from 
+   http://ewontfix.com/14/
+   https://github.com/strake/init/blob/master/init.c
+*/
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main () {
 	sigset_t set;
 	pid_t pid;
-	int c;
+	int status;
 	
-	if (getpid () != 1) return 1;
+	if (getpid () != 1) return EXIT_FAILURE;
 	
 	sigfillset (&set);
 	sigprocmask (SIG_BLOCK, &set, 0);
 	
 	pid = fork ();
-	if (pid < 0) return 1;
-	if (pid > 0) for (;;) wait (&c); /* orphans */
+	if (pid < 0) return EXIT_FAILURE;
+	if (pid > 0) for (;;) wait (&status); /* orphans */
 	
 	sigprocmask (SIG_UNBLOCK, &set, 0);
 	
 	setsid ();
 	setpgid (0, 0);
-	return execve ("/sbin/go", (char * []){ "go", 0 }, (char * []){ 0 });
+	return execve ("/etc/rc", (char * []){ "rc", 0 }, (char * []){ 0 });
 }
