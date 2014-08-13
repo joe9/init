7b4ee1446f55c1f44e4788edbb5a91ead94979ac
joe9 <joe9mail@gmail.com>
2014-08-13 11:50:16 -0500

    added path to all commands in shutdown scripts

--- asynx_spawn.c
+++ asynx_spawn.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+/* #include <stdio.h> */
 
 int main (int argc, char *argu[]) {
 	pid_t pid;
@@ -10,14 +11,19 @@ int main (int argc, char *argu[]) {
 /* init.c:16:15: warning: unused parameter ‘argc’ [-Wunused-parameter] */
 	(void) argc;
 
+/* 	http://www.cs.cityu.edu.hk/~lwang/fork */
+/* 	printf("first fork\n"); */
 	pid = fork ();
 	if (pid < 0) return EXIT_FAILURE;
 	if (pid > 0) {
 		waitpid (pid, &c, 0);
+/* 		printf("parent exiting\n"); */
 		return c;
 	}
+/* 	printf("second fork\n"); */
 	pid = fork ();
 	if (pid < 0) return 1;
 	if (pid > 0) return 0;
+/* 	printf("after double fork, starting %s\n",argu[1]); */
 	return execvp (argu[1], argu + 1);
 }
