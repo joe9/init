9eb6ec9e5648ab6e339d7d2556fcd5a1247357a0
joe9 <joe9mail@gmail.com>
2014-08-24 15:45:21 -0500

    using sigwait makes the code beautiful

--- init.c
+++ init.c
@@ -1,4 +1,3 @@
-
 /* got the below code from
    http://ewontfix.com/14/
    https://github.com/strake/init/blob/master/init.c */
@@ -11,107 +10,99 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-static pid_t rc_pid     = 0;
-static int hup_received = 0;
-void signal_handler (int sig) {
-   if (SIGHUP == sig) hup_received = 1;
-   if (0 < rc_pid)   kill(rc_pid,sig);
+#define LEN(x) (sizeof (x) / sizeof *(x))
+
+void sigpropogate (pid_t rc_pid,int sig);
+void sigreap      (pid_t rc_pid,int sig);
+void sigrestart   (pid_t rc_pid,int sig);
+
+static struct {
+   int signal;
+   void (*handler)(pid_t rc_pid, int sig);
+} sigmap[] = {
+	{ SIGUSR1, sigpropogate },
+	{ SIGUSR2, sigpropogate },
+	{ SIGCHLD, sigreap      },
+	{ SIGHUP,  sigrestart   }
+};
+static char *const rcinitcmd[] = { "/home/j/dev/scripts/init/hello", NULL };
+
+void sigpropogate (pid_t rc_pid,int sig) {
+   if (0 < rc_pid) kill(rc_pid,sig);
 }
-void waitloop(char * const *envp) {
-    sigset_t set;
-    pid_t wait_pid;
-    char pid_str[15];
-    while ((wait_pid = wait(0))) { /* orphans */
-       /* when there are no children, do not waste cpu cycles */
-       if (-1 == wait_pid && ECHILD == errno) sleep(1);
-       /* if the /etc/rc process ends, do not bother signalling it */
-       else if (rc_pid == wait_pid) {
-	   /* do not bother with signalling as rc
-	    * child does not exist anymore */
-	   rc_pid = 0;
-	   sigfillset (&set);
-	   sigdelset (&set, SIGHUP);
-	   sigprocmask (SIG_BLOCK, &set, 0);
-       }
-       if (1 == hup_received) { 
-	   sprintf(pid_str, "%d", rc_pid);
-           /* execve ( "/home/j/dev/scripts/init/init" */
-	   execve ( "/sbin/init"
-		  , (char * []){ "init",pid_str,0 }
-		  , envp );
-       }
+void sigreap (pid_t rc_pid,int sig) {
+   pid_t wait_pid = 0;
+   /* to avoid warning: unused parameter ‘sig’ [-Wunused-parameter] */
+   (void)sig;
+   printf("sigreap called\n");
+   while (0 < (wait_pid = waitpid(WAIT_ANY, NULL, WNOHANG))) {
+	if (rc_pid == wait_pid) {
+	    /* do not bother with signalling as rc
+	     * child does not exist anymore */
+	    rc_pid = 0;
+	}
+   }
+}
+void sigrestart (pid_t rc_pid,int sig) {
+   char pid_str[15];
+   if (0 < rc_pid)   kill(rc_pid,sig);
+   sprintf(pid_str, "%d", rc_pid);
+   execv ("/home/j/dev/scripts/init/init"
+/*    execve ( "/sbin/init" */
+	   , (char * []){ "init",pid_str,0 });
+}
+pid_t spawn(char *const argv[], char * const *envp) {
+    pid_t rc_pid = 0;
+    int savederrno = 0;
+    /* http://www.cs.cityu.edu.hk/~lwang/fork */
+    rc_pid = fork();
+    if (0 > rc_pid) perror("fork");
+    else if (rc_pid == 0) { /* child */
+	setsid ();
+	setpgid (0, 0);
+	/* return execve ("/etc/rc", (char * []){ "rc", 0 }, (char * []){ 0 }); */
+	/* return execve ("/etc/rc", (char * []){ "rc", 0 }, envp); */
+	execve (argv[0],argv,envp);
+/* 	    execve ("/etc/rc", (char * []){ "rc", 0 }, envp); */
+	perror("init: execve /etc/rc");
+	execve ("/bin/sh", (char * []){ "sh", 0 }, envp);
+	savederrno = errno;
+	perror("init: execve /bin/sh");
+	_exit(savederrno);
     }
+    return rc_pid;
 }
 /* using envp as linux kernel sets TERM environment variable which is
  * used by the rc init scripts to figure out if it is a colour
  * terminal. got the below line from runit-init.c */
 int main (int argc, const char * const *argv, char * const *envp) {
-    struct sigaction action;
     sigset_t set;
-    int savederrno = 0;
+    int sig = 0;
+    size_t i = 0;
+    pid_t rc_pid = 0;
 	
-    if (getpid () != 1) return EXIT_FAILURE;
+/*     if (getpid () != 1) return EXIT_FAILURE; */
+/*     chdir("/"); */
 
     /* block all signals */
     sigfillset (&set);
     sigprocmask (SIG_BLOCK, &set, 0);
 
-    /* setup and unblock the HUP signal handler */
-    action.sa_handler = signal_handler;
-    sigemptyset (&action.sa_mask);
-    action.sa_flags = 0;
-    sigaction (SIGHUP, &action, NULL);
-    /* Unmask only HUP signal */
-    sigemptyset (&set);
-    sigaddset (&set, SIGHUP);
-    sigprocmask (SIG_UNBLOCK, &set, 0);
-
-    if (2 == argc && 0 < atoi(argv[1]))
-       rc_pid = atoi(argv[1]);
+    if (2 == argc && 0 < atoi(argv[1])) rc_pid = atoi(argv[1]);
     printf( "argc: %d, argv[0]: %s, argv[1]: %s pid: %d\n"
        , argc,argv[0],argv[1],rc_pid);
-    if (2 == argc) waitloop(envp);
+    if (1 == argc) rc_pid = spawn(rcinitcmd,envp);
 	
-    /* We need to block signals until we have forked */
-    sigfillset (&set);
-    sigprocmask (SIG_BLOCK, &set, 0);
-	
-    /* http://www.cs.cityu.edu.hk/~lwang/fork */
-    rc_pid = fork ();
-    if (0 > rc_pid) return EXIT_FAILURE;
-	/* init doesn't want to get signals, hence not unblocking */
-    /* if (rc_pid > 0) for (;;) wait (&status); /\* orphans *\/ */
-    if (0 < rc_pid) {
-	/* setup the signal handler to progorate signals to
-	 * children */
-	 action.sa_handler = signal_handler;
-	 sigemptyset (&action.sa_mask);
-	 action.sa_flags = 0;
-	 sigaction (SIGUSR1, &action, NULL);
-	 sigaction (SIGUSR2, &action, NULL);
-	 sigaction (SIGHUP, &action, NULL);
-	/* Unmask only USR1 and USR2 signals */
-	 sigemptyset (&set);
-	 sigaddset (&set, SIGUSR1);
-	 sigaddset (&set, SIGUSR2);
-	 sigaddset (&set, SIGHUP);
-	 sigprocmask (SIG_UNBLOCK, &set, 0);
-	 waitloop(envp);
-	 /* control never comes here */
+    while (1) {
+	sigwait(&set, &sig);
+	for (i = 0; i < LEN(sigmap); i++) {
+	    if (sigmap[i].signal == sig) {
+	       sigmap[i].handler(rc_pid,sig);
+	       break;
+	    }
+	}
     }
-    /* for child */
-    /* Unmask signals */
-    sigprocmask (SIG_UNBLOCK, &set, 0);
-
-    setsid ();
-    setpgid (0, 0);
-    /* return execve ("/etc/rc", (char * []){ "rc", 0 }, (char * []){ 0 }); */
-    /* return execve ("/etc/rc", (char * []){ "rc", 0 }, envp); */
-    /* execve ("/home/j/dev/scripts/init/hello", (char * []){ "hello", 0 }, envp); */
-    execve ("/etc/rc", (char * []){ "rc", 0 }, envp);
-    perror("init: execve /etc/rc");
-    execve ("/bin/sh", (char * []){ "sh", 0 }, envp);
-    savederrno = errno;
-    perror("init: execve /bin/sh");
-    _exit(savederrno);
+    /* not reachable */
+    return EXIT_SUCCESS;
 }
+
