74287f776149f78441f2afb82bd76c4a257c1a33
joe9 <joe9mail@gmail.com>
2014-08-26 18:23:47 -0500

    replaced MVar with Chan

--- hsvc.hs
+++ hsvc.hs
@@ -1,34 +1,16 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# LANGUAGE DeriveDataTypeable    #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TemplateHaskell       #-}
 
--- import           Control.Concurrent.MVar
--- import           Control.Exception.Extensible (SomeException (..),
---                                                bracket, finally,
---                                                fromException, throw,
---                                                try)
--- import qualified Control.Exception.Extensible as E
--- import           Control.Monad
--- import           Data.Function
--- import           Data.Maybe           (isJust)
--- import           Debug.Trace
--- import           System.Directory
--- import           System.FilePath
--- import           System.Info
--- import           System.IO
--- import           System.Posix.IO
--- import           System.Posix.Unistd
--- import           System.Process
 import           Control.Concurrent
 import           Data.Default
 import           Safe
 import           System.Environment
 import           System.Exit
-import           System.Posix.Process (executeFile, forkProcess,
-                                       getAnyProcessStatus)
-import           System.Posix.Process (ProcessStatus)
+import           System.Posix.Process (ProcessStatus, executeFile,
+                                       forkProcess,
+                                       getAnyProcessStatus,
+                                       getProcessID)
 import           System.Posix.Signals
 import           System.Posix.Types   (ProcessID)
 import           Text.Groom
@@ -43,56 +25,46 @@ data Args = Args { aDelay        :: Int
 
 instance Default Args where def = Args 0 True 0 "" [] False
 
-type Hup   = Bool
-type Term  = Bool
-type Child = Bool
-
 main :: IO ()
 main = do
     getArgs >>= (\a -> putStrLn ("program started with " ++ groom a))
     getProcessID >>= (\a -> putStrLn ("process id: " ++ groom a))
-    args <- (getArgs >>= parse def)
+    args <- getArgs >>= parse def
     putStrLn ("program started with " ++ groom args)
     pid <- runOnce args
     putStrLn "after 1st runOnce"
     let pargs = args{aChildPid = pid}
-    h <- newMVar False
-    t <- newMVar False
-    c <- newMVar False
-    installSignalHandlers h t c
+    c <- newChan
+    installSignalHandlers c
     putStrLn "before loop"
-    loop h t c pargs
+    loop c pargs
 
-loop :: MVar Hup -> MVar Term -> MVar Child -> Args -> IO ()
-loop h t c a =
-    apply restart h a -- when hupped
-    >>= apply term t  -- when termed
-    >>= apply reap c  -- when child
-    >>= respawn -- other checks
-    >>= (\g -> signalWait >> loop h t c g)
+loop :: Chan Signal -> Args -> IO ()
+loop c a =
+    readChan c >>= processSignal a >>= respawn >>= loop c
+
+processSignal :: Args -> Signal -> IO Args
+processSignal a s
+   | s == sigHUP  = restart a -- when hupped
+   | s == sigTERM = term a -- when termed
+   | s == sigCHLD = reap a -- when child
+   | otherwise = error "processSignal: unknown signal"
 
 respawn :: Args -> IO Args
 respawn a | aChildPid a > 0 = return a
-          | aChildPid a == 0 && aRunOnce a == True    =
+          | aChildPid a == 0 && aRunOnce a =
               exitSuccess >> return a
-          | aChildPid a == 0 && aTermReceived a == True =
+          | aChildPid a == 0 && aTermReceived a =
               exitSuccess >> return a
-          | aChildPid a == 0 = threadDelay ((aDelay a) * 1000000)
+          | aChildPid a == 0 = threadDelay (aDelay a * 1000000)
                               >> runOnce a
                               >>= (\p -> return a{aChildPid = p})
           | otherwise = error "respawn: negative child pid"
 
-apply :: (Args -> IO Args) -> MVar Bool -> Args -> IO Args
-apply f m a = swapMVar m False >>= ifTrue f a
-
-ifTrue :: (Args -> IO Args) -> Args -> Bool -> IO Args
-ifTrue f a True = f a
-ifTrue _ a _    = return a
-
 reap :: Args -> IO Args
 reap a = do
     p <- getAnyProcessStatus True True
-    putStrLn $ "getAnyProcessStatus returned: " ++ (groom p)
+    putStrLn $ "getAnyProcessStatus returned: " ++ groom p
     let args = a{aChildPid = removePid (aChildPid a) p}
     return args
 
@@ -103,12 +75,11 @@ term a | aChildPid a > 0 =
 
 restart :: Args -> IO Args
 restart a = do
-    putStrLn . show $ a
-    env <- getEnvironment
+    print a
     f <- getExecutablePath
     putStrLn $ "restart: file is  " ++ f
     putStrLn $ "restart: args are " ++ groom args
-    _ <- executeFile f False args . Just $ env
+    run f args
     return a
     where args = filter (not . (==) "")
                    [ "-d", show $ aDelay a
@@ -120,18 +91,6 @@ restart a = do
           runOnceArg True  = "-o"
           runOnceArg False = ""
 
--- https://ghc.haskell.org/trac/ghc/wiki/Commentary/Rts/Signals
-signalWait :: IO ()
-signalWait = do
-    putStrLn "signalWait"
-    -- https://ghc.haskell.org/trac/ghc/ticket/4504
-    blockSignals reservedSignals
-    awaitSignal Nothing >> yield
-    putStrLn "after signalWait"
-    threadDelay (1 * 1000000)
-    yield
-    putStrLn "end signalWait"
-
 runOnce :: Args -> IO ProcessID
 runOnce a@(Args{aChildPid = 0}) =
  forkProcess $ run (aCommand a) (aCommandArgs a)
@@ -140,26 +99,18 @@ runOnce a = return . aChildPid $ a
 run :: FilePath -> [String] -> IO ()
 run c args = getEnvironment >>= executeFile c False args . Just
 
-installSignalHandlers :: MVar Hup -> MVar Term -> MVar Child -> IO ()
-installSignalHandlers h t c =
-    mapM_ installSignalHandler  [ (sigHUP, h)
-                                , (sigTERM, t)
-                                , (sigCHLD, c)
-                                ]
+installSignalHandlers :: Chan Signal -> IO ()
+installSignalHandlers c =
+    mapM_ (installSignalHandler c)  [ sigHUP , sigTERM , sigCHLD ]
 
-installSignalHandler :: (Signal, MVar Bool) -> IO Handler
-installSignalHandler (s,m) =
- installHandler s (Catch $ toTrue m)  Nothing
-
-toTrue :: MVar Bool -> IO ()
-toTrue m = takeMVar m >> putMVar m True
+installSignalHandler :: Chan Signal -> Signal -> IO Handler
+installSignalHandler c s =
+ installHandler s (Catch $ writeChan c s)  Nothing
 
 removePid :: ProcessID -> Maybe (ProcessID, ProcessStatus) -> ProcessID
 removePid 0 _ = 0
 removePid p Nothing = p
-removePid p (Just (pid,_)) = case (pid == p) of
-                               True  -> 0
-                               False -> p
+removePid p (Just (pid,_)) = if pid == p then 0 else p
 
 sendTerm :: ProcessID -> IO ()
 sendTerm = sendSignal softwareTermination
@@ -169,7 +120,7 @@ sendTerm = sendSignal softwareTermination
 
 sendSignal :: Signal -> ProcessID -> IO ()
 -- sendSignal sig ps = mapM_ (signalProcess sig) ps
-sendSignal sig = signalProcess sig
+sendSignal = signalProcess
 
 parse :: Args -> [String] -> IO Args
 parse _ []             = usage >> exitSuccess
@@ -210,3 +161,14 @@ version = putStrLn "svc 0.1"
 --     >> installHandler sigUSR1 Default Nothing
 --     >> installHandler sigUSR2 Default Nothing
 --     >> return ()
+-- https://ghc.haskell.org/trac/ghc/wiki/Commentary/Rts/Signals
+-- signalWait :: MVar Hup -> MVar Term -> MVar Child -> IO ()
+-- signalWait h t c = do
+--     putStrLn "signalWait"
+--     -- https://ghc.haskell.org/trac/ghc/ticket/4504
+--     blockSignals reservedSignals
+--     awaitSignal Nothing >> yield
+-- --     putStrLn "after signalWait"
+-- --     threadDelay (1 * 1000000)
+-- --     yield
+--     putStrLn "end signalWait"
